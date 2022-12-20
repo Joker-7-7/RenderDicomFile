7c43acb5c3396ba048edab245db3e4fdcb7d86f7
George Gevorgyan <g_george@dentalbot.ru>
2022-12-20 12:39:10 +0500

    Added function for model parameters.

---
+++ Src/LayersConfiguration.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vtkColorTransferFunction.h>
+#include <vtkPiecewiseFunction.h>
+#include <vtkVolumeProperty.h>
+
+namespace LayersConfiguration
+{
+	/// <summary>
+	/// Parameters WW and WL
+	/// </summary>
+	struct setupWLWW
+	{
+		double wl = 900.0;
+		double ww = 300.0;
+	};
+
+	/// <summary>
+	/// Function to set color function and opacity function for volume model
+	/// </summary>
+	/// <param name="volume_property"></param>
+	/// <param name="wl"></param>
+	/// <param name="ww"></param>
+	void setColorAndOpacityFunction(vtkVolumeProperty* volumeProperty_, double wl_, double ww_);
+}
